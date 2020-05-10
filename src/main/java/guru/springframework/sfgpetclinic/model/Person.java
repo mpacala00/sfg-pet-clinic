4c12c1e9c58b5ac605e17fb572071c34f81a1dbf
miki <mpacala00@gmail.com>
2020-05-10 14:01:10 +0200

    added POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
