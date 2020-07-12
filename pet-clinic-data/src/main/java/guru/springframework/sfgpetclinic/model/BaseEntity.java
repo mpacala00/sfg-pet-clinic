78609b24692df0be1a0ca35b09261c3e033d6788
mpacala00 <mpacala00@gmail.com>
2020-07-12 15:40:17 +0200

    Adding @MappedSuperclass and @Id, @GeneratedValue to BaseEntity. Closes #35.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,11 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass //means that its the base class for JPA that other classes will be inheriting from
+//but we don't need this specific class put into database
 public class BaseEntity implements Serializable {
 
     //Hibernate recommends using Long instead of long
     //Box types can be null
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
