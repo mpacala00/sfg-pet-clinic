daf4d0680765ecb19f4cfe711bec72003154f359
mpacala00 <mpacala00@gmail.com>
2020-07-12 16:48:49 +0200

    Mapping Vet and Specialty as Entities. Using @ManyToMany between them. Closes #37.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,10 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER) //JPA will try to load everything all at once
+    //by default it wouldn't load it until it is asked for
+    @JoinTable(name = "vet_specialties",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Vet(String firstName, String lastName) {
