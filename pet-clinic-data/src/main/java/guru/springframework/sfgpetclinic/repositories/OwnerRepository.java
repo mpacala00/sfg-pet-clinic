d60bf5f3de18b85e37c75f4aed8660dcf981f539
mpacala00 <mpacala00@gmail.com>
2020-07-12 17:17:35 +0200

    Adding JPA repositories. Closes #39.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+//spring data JPA will provide an implementation of these repos at runtime, which is nice
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    //an example of a query method:
+    //Owner findByLastName(String lastName);
+}
