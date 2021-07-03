6992e392367ad516ebc473f9a073bedb7cca5422
mpacala00 <mpacala00@gmail.com>
2021-07-03 18:59:36 +0200

    Adding owner templates, mapping for displaying owner by id and a test. Closes #53

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,8 +3,11 @@ package guru.springframework.sfgpetclinic.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.springframework.web.servlet.ModelAndView;
 
 //only accessible directory is /owners
 @RequestMapping("/owners")
@@ -31,5 +34,12 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners() {
         return "notImplemented.html";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOnwer(@PathVariable Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -11,12 +11,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.isNotNull;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -53,7 +56,7 @@ class OwnerControllerTest {
     void listOwners(String urlPath) throws Exception {
         when(ownerService.findAll()).thenReturn(owners);
 
-        mockMvc.perform(org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get(urlPath))
+        mockMvc.perform(MockMvcRequestBuilders.get(urlPath))
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/index"))
                 .andExpect(model().attribute("owners", hasSize(2)));
@@ -61,10 +64,20 @@ class OwnerControllerTest {
 
     @Test
     void findOwners() throws Exception {
-        mockMvc.perform(org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get("/owners/find"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners/find"))
                 .andExpect(view().name("notImplemented.html"))
                 .andExpect(status().isOk());
 
         verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void diaplyOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 }
