ddd2df1af02c72f0f68d06bf911c6f24d4bef1c5
mpacala00 <mpacala00@gmail.com>
2021-07-06 17:32:44 +0200

    Adding visit controller and mappings. Closes #57

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -16,12 +16,16 @@ import java.util.Set;
 public class Pet extends BaseEntity{
 
     @Builder
-    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate) {
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
         super(id);
         this.name = name;
         this.petType = petType;
         this.owner = owner;
         this.birthDate = birthDate;
+
+        if(visits != null && visits.size() > 0) {
+            this.visits = visits;
+        }
     }
 
     @Column(name = "name")
---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VisitController.java
@@ -0,0 +1,64 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import javax.validation.Valid;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+class VisitController {
+
+    private final VisitService visitService;
+    private final PetService petService;
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable("petId") Long petId, Model model) {
+        Pet pet = petService.findById(petId);
+        model.addAttribute("pet", pet);
+
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        visit.setPet(pet);
+        return visit;
+    }
+
+    // Spring MVC calls method loadPetWithVisit(...) before initNewVisitForm is called
+    @GetMapping("/owners/*/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable Long petId, Model model) {
+        return "pets/createOrUpdateVisitForm";
+    }
+
+    // Spring MVC calls method loadPetWithVisit(...) before processNewVisitForm is called
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(@PathVariable Long ownerId, @Valid Visit visit, BindingResult result) {
+        if (result.hasErrors()) {
+            return "pets/createOrUpdateVisitForm";
+        }
+        else {
+            this.visitService.save(visit);
+            return "redirect:/owners/" + ownerId;
+        }
+    }
+
+}
